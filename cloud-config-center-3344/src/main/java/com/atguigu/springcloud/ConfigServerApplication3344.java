833cb04fa24fb4dcda90be89919a8b060140d9d9
LiDongYang-coder <3295224486@qq.com>
2020-12-07 22:58:43 +0800

    配置中心

--- cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigServerApplication3344.java
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigServerApplication3344.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication3344 {
+    // 配置消息总线时用
+    //curl -X POST "http://localhost:3344/actuator/bus-refresh"
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication3344.class,args);
     }
